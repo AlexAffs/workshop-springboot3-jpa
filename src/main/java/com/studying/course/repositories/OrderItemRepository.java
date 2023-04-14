a7f71e15bd871efef5af375d84b1f21eaa28a8ea
Alexandre Raphael <alexaffs1@gmail.com>
2023-04-14 05:56:04 -0300

    OrderItem, many-to-many association with exra attributes

---
+++ src/main/java/com/studying/course/repositories/OrderItemRepository.java
@@ -0,0 +1,9 @@
+package com.studying.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.studying.course.entities.OrderItem;
+
+public interface OrderItemRepository extends JpaRepository<OrderItem, Long>{
+	
+}
